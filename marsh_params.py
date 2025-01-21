748419e981bd2a4b5801a5c77327285c7528aa81
dzauski585 <dzauski585@gmail.com>
2025-01-20 20:00:56 -0500

    updated models

--- marsh_params.py
+++ marsh_params.py
@@ -3,15 +3,18 @@ Marsh B, White M, Morton N, Kenny GN. Pharmacokinetic model driven infusion of p
 
 https://pubmed.ncbi.nlm.nih.gov/1859758/
 '''
+def marsh_params():
+    v1 = 0.228 #L/kg
+    v2 = 0.463 # L/kg
+    v3 = 2893 # L/kg
 
+    k10 = 0.119 #min^-1 (k10 = K10 + K1e)
+    k12 = 0.112 #min^-1
+    k13 = 0.042 #min^-1
+    k21 = 0.055 #min^-1
+    k31 = 0.0033 #min^-1
+    ke0 = 0.26 #min^-1
 
-v1 = 0.228 #L/kg
-v2 = 0.463 # L/kg
-v3 = 2893 # L/kg
+    marsh_params = [v1, v2, v3, k10, k12, k13, k21, k31, ke0]
 
-k10 = 0.119 #min^-1 (k10 = K10 + K1e)
-k12 = 0.112 #min^-1
-k13 = 0.042 #min^-1
-k21 = 0.055 #min^-1
-k31 = 0.0033 #min^-1
-ke0 = 0.26 #min^-1
+    return marsh_params
